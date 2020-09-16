034083825ea0943c1f51e44d2e463fc2d08ad3c3
Pablo Ochoa <pochoa79@gmail.com>
2020-09-16 13:29:24 -0500

    Ajuste de todo el proyecto

--- src/main/java/com/platzi/market/domain/dto/AuthenticationRequest.java
+++ src/main/java/com/platzi/market/domain/dto/AuthenticationRequest.java
@@ -5,12 +5,11 @@ public class AuthenticationRequest {
     private String username;
     private String password;
 
-
     public String getUsername() {
         return username;
     }
 
-    public void setUserName(String username) {
+    public void setUsername(String username) {
         this.username = username;
     }
 
